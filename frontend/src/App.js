c2034a5a1dbd5fbe1c58d5c66182025a29f94338
Luiz Fernando Costa <luiz_batera_9@hotmail.com>
2025-03-25 17:27:23 -0300

    Front Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Toaster, toast } from "sonner";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +26,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Toaster />
+      </div>
     </div>
   );
 }
